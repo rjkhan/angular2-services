b5f3608b7d4835fb489e84d82c5c0ca6e39bcf4c
rabnawaz jansher <rabnawazjansher8@gmail.com>
2017-07-22 00:04:50 +0500

    get request http

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import {Http} from '@angular/http';
 @Injectable()
 export class WeatherService {
 
-  constructor() { }
+  private url: string;
+  private response; 
+  constructor(private _http: Http) { }
 
-  getData()
+  getData(searchValue ="pakistan")
   {
-    return "data service method call";
+    this.url = "http://api.openweathermap.org/data/2.5/weather?q="+ searchValue +"&appid=e8f3a3a87cd6064649e142b3764540f7";
+    this.response = this._http.get(this.url);
+    return this.response;
   }
 
 }
