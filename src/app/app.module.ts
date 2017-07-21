77d933fb2b3f34edc0c522c3fd3850ad7ecb4bdb
rabnawaz jansher <rabnawazjansher8@gmail.com>
2017-07-21 23:50:54 +0500

    http module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { WeathersComponent } from './weathers/weathers.component';
     WeathersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
